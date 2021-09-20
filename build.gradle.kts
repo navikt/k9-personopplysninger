f49691ac59ee88d0609f25cf29af5a5f57d35311
albrektsson <emil.albrektsson@nav.no>
2021-09-20 10:54:36 +0200

    Dependency bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val junitJupiterVersion = "5.7.2"
-val k9rapidVersion = "1.20210625095239-653e3a9"
-val ktorVersion = "1.6.1"
-val dusseldorfKtorVersion = "2.1.6.0-1516d10"
+val junitJupiterVersion = "5.8.0"
+val k9rapidVersion = "1.20210920084849-2ac86f1"
+val ktorVersion = "1.6.3"
+val dusseldorfKtorVersion = "2.1.6.3-bbb6831"
 val jsonassertVersion = "1.5.0"
 val orgJsonVersion = "20210307"
 val mockkVersion = "1.12.0"
@@ -14,7 +14,7 @@ val assertjVersion = "3.20.2"
 val mainClass = "no.nav.omsorgspenger.AppKt"
 
 plugins {
-    kotlin("jvm") version "1.5.20"
+    kotlin("jvm") version "1.5.30"
     id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
@@ -91,7 +91,7 @@ tasks {
     }
 
     withType<Wrapper> {
-        gradleVersion = "7.1.1"
+        gradleVersion = "7.2"
     }
 
 }
