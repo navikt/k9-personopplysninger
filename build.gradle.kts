6d24ad2eb691cef36e4acd76f3d287eada7e51bd
Emil Albrektsson <61695840+albrektsson@users.noreply.github.com>
2023-06-19 07:34:01 +0200

    Dep bumps (#184)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val junitJupiterVersion = "5.9.3"
-val k9rapidVersion = "1.20230426095941-a725ff7"
-val ktorVersion = "2.3.0"
-val dusseldorfKtorVersion = "3.2.3.0-7a92774"
+val k9rapidVersion = "1.20230601082432-b338e04"
+val ktorVersion = "2.3.1"
+val dusseldorfKtorVersion = "3.2.3.1-b31dd02"
 val jsonassertVersion = "1.5.1"
 val orgJsonVersion = "20230227"
 val mockkVersion = "1.13.5"
@@ -13,9 +13,9 @@ val assertjVersion = "3.24.2"
 val mainClass = "no.nav.omsorgspenger.AppKt"
 
 plugins {
-    kotlin("jvm") version "1.8.21"
+    kotlin("jvm") version "1.8.22"
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("org.sonarqube") version "4.0.0.2929"
+    id("org.sonarqube") version "4.2.1.3168"
     jacoco
 }
 
